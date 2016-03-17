6fb0e7d1569245878652aedbd17685490b6077ca
rajeshkulandaivel <@rkulandaivel@yahoo.com>
2016-03-17 14:51:36 +0000

    rajesh

--- Demo.py
+++ Demo.py
@@ -53,7 +53,8 @@ def FaceIdentify(groupid, faceid):
     found_conf = False
     print "In FaceIdentify" 
     conn = httplib.HTTPSConnection('api.projectoxford.ai')    
-    body="{\"personGroupId\":\"123456\",\"faceIds\":[ + faceid +],\"maxNumOfCandidatesReturned\":1}";
+    body="{\"personGroupId\":\"123456\",\"faceIds\""
+    body = body + ":[" + faceid + "],\"maxNumOfCandidatesReturned\":1}";
     print body
     conn.request("POST", "/face/v1.0/identify" , body, headers)
     response = conn.getresponse()
